14d9e9aa8cb8c17bf93d163b51aa76a3f819e686
aroberts2603 <aroberts2603@gmail.com>
2019-01-14 14:43:55 -0700

    added the harder - optional bonus for 2019-1-14

--- January 2019/2019-1-14-Easy.py
+++ January 2019/2019-1-14-Easy.py
@@ -12,6 +12,7 @@ def balanced(str):
 	else:
 		return False
 
+print("Basic:")
 print(balanced("xxxyyy"))#true
 print(balanced("yyyxxx"))#true
 print(balanced("xxxyyyy"))#false
@@ -19,3 +20,33 @@ print(balanced("yyxyxxyxxyyyyxxxyxyx"))#true
 print(balanced("xyxxxxyyyxyxxyxxyy"))#false
 print(balanced(""))#true
 print(balanced("x"))#false
+
+# Optional Bonus - Given a string containing only lowercase letters
+# find whether every letter that appears in the string appears the same number of times.
+
+def advBalanced(string):
+	letters = []
+	counters = []
+	for i in range(len(string)):
+		if(not string[i] in letters):
+			letters.append(string[i])
+			counters.append(1)
+		else:
+			counters[letters.index(string[i])] += 1
+
+	r = True
+	for i in range(len(counters)-1):
+		if(counters[i] != counters[i+1]):
+			r = False
+
+	return r
+
+print("Advanced:")
+print(advBalanced("xxxyyyzzz"))#True
+print(advBalanced("abccbaabccba"))#True
+print(advBalanced("xxxyyyzzzz"))#False
+print(advBalanced("abcdefghijklmnopqrstuvwxyz"))#True
+print(advBalanced("pqq"))#False
+print(advBalanced("fdedfdeffeddefeeeefddf"))#False
+print(advBalanced("www"))#True
+print(advBalanced(""))#True
